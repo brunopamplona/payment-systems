c9dd2c2c7584ecc5f4f3fea74f72923f5e220974
brunopamplona <bruno.cortereal@gmail.com>
2023-03-09 14:21:34 -0300

    removing invalidParameterError dependencies

--- clean-node-rest-api/presentation/routers/login-router.spec.js
+++ clean-node-rest-api/presentation/routers/login-router.spec.js
@@ -2,7 +2,6 @@ const LoginRouter = require('./login-router')
 const MissingParamError = require('../helpers/missing-param-error')
 const UnauthorizedError = require('../helpers/unauthorized-param-error')
 const ServerError = require('../helpers/server-error')
-const InvalidParamError = require('../helpers/invalid-param-error')
 
 const makeSut = () => {
     const emailValidatorSpy = makeEmailValidator()
